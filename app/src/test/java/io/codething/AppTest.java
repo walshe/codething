1f4db15a4b9cefb356228106d5afe2b20474ceea
navery-max <navery@marketaxess.com>
2024-06-04 12:22:31 +0100

    updated

--- app/src/test/java/io/codething/AppTest.java
+++ app/src/test/java/io/codething/AppTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
-import org.hamcrest.MatcherAssert.assertThat;
 import org.hamcrest.Matchers.*;
 
 class TrackerAppTest {
